e7d7534c8d0477a52abe6d4cefe9d8c6631d3ba4
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-02-21 13:00:13 -0300

    chore: removes all files and folders

--- js/script.js
+++
@@ -1,7 +0,0 @@
-window.onscroll = function () {
-  if (document.documentElement.scrollTop > 100) {
-    document.querySelector(".go-top-container").classList.add("show");
-  } else {
-    document.querySelector(".go-top-container").classList.remove("show");
-  }
-};
